7071da3a8d6f283fd2892d000f44a4830bd9523c
VietoPlagueNox <dawsongriffon05@gmail.com>
2024-04-18 16:56:02 +0200

    Modification

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-SESSION_START();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -103,204 +99,18 @@ SESSION_START();
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
 
-                        <!-- Nav Item - Alerts -->
-                        <li class="nav-item dropdown no-arrow mx-1">
-                            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-bell fa-fw"></i>
-                                <!-- Counter - Alerts -->
-                                <span class="badge badge-danger badge-counter">3+</span>
-                            </a>
-                            <!-- Dropdown - Alerts -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="alertsDropdown">
-                                <h6 class="dropdown-header">
-                                    Alerts Center
-                                </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-primary">
-                                            <i class="fas fa-file-alt text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 12, 2019</div>
-                                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-success">
-                                            <i class="fas fa-donate text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 7, 2019</div>
-                                        $290.29 has been deposited into your account!
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-warning">
-                                            <i class="fas fa-exclamation-triangle text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 2, 2019</div>
-                                        Spending Alert: We've noticed unusually high spending for your account.
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-                            </div>
-                        </li>
-
-                        <!-- Nav Item - Messages -->
-                        <li class="nav-item dropdown no-arrow mx-1">
-                            <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-envelope fa-fw"></i>
-                                <!-- Counter - Messages -->
-                                <span class="badge badge-danger badge-counter">7</span>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="messagesDropdown">
-                                <h6 class="dropdown-header">
-                                    Message Center
-                                </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_1.svg"
-                                            alt="...">
-                                        <div class="status-indicator bg-success"></div>
-                                    </div>
-                                    <div class="font-weight-bold">
-                                        <div class="text-truncate">Hi there! I am wondering if you can help me with a
-                                            problem I've been having.</div>
-                                        <div class="small text-gray-500">Emily Fowler · 58m</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_2.svg"
-                                            alt="...">
-                                        <div class="status-indicator"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">I have the photos that you ordered last month, how
-                                            would you like them sent to you?</div>
-                                        <div class="small text-gray-500">Jae Chun · 1d</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_3.svg"
-                                            alt="...">
-                                        <div class="status-indicator bg-warning"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">Last month's report looks great, I am very happy with
-                                            the progress so far, keep up the good work!</div>
-                                        <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"
-                                            alt="...">
-                                        <div class="status-indicator bg-success"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">Am I a good boy? The reason I ask is because someone
-                                            told me that people say this to all dogs, even if they aren't good...</div>
-                                        <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
-                            </div>
-                        </li>
-
-                        <div class="topbar-divider d-none d-sm-block"></div>
 
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                                    
-                                <?php
-                                    // Vérifier si une session est déjà active avant de la démarrer
-                                    if(session_status() !== PHP_SESSION_ACTIVE) {
-                                        session_start();
-                                    }
-
-                                    // Récupération de l'email depuis la session
-                                    $email = $_SESSION['email'];
-
-                                    // Connexion à la base de données
-                                    $connection = mysqli_connect("172.16.136.9", "root", "root", "logiciel_stages");
-
-                                    // Vérifier la connexion
-                                    if (!$connection) {
-                                        die("La connexion a échoué : " . mysqli_connect_error());
-                                    }
-
-                                    // Requête SQL
-                                    $query = "SELECT prenom_u FROM tbl_user WHERE mail_u='$email'";
-                                    $result = mysqli_query($connection, $query);
-
-                                    // Vérifier si la requête a abouti
-                                    if (!$result) {
-                                        die("Erreur dans la requête : " . mysqli_error($connection));
-                                    }
-
-                                    // Affichage des données
-                                    $row = mysqli_fetch_assoc($result);
-                                    if ($row) {
-                                        echo $row['prenom_u'];
-                                    } else {
-                                        echo "Aucun prénom trouvé.";
-                                    }
-
-                                    // Libérer la mémoire des résultats
-                                    mysqli_free_result($result);
-
-                                    // Fermer la connexion à la base de données
-                                    mysqli_close($connection);
-                                ?>
-
-
-                                </span>
-
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Moi</span>
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -310,10 +120,7 @@ SESSION_START();
                                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Settings
                                 </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a>
+
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -331,49 +138,53 @@ SESSION_START();
 
                 </nav>
 
-                <a> Bienvenue sur le site ! <a>
-                    <br> </br>
+<body>
 
-                    <a>Petite présention :</a> 
-                    <br>  </br>
-                    <a>Ce site à était conçue par une équipe de trois étudiants en BTS Services Informatiques aux Organisations (SIO) option Solutions Logicielles et Applications Métiers (SLAM).</a>
-                    <br> </br>
+<a>Bienvenue sur le site !</a>
+<br><br>
 
-                    <a> Stage à venir : </a>
+<a>Petite présentation :</a>
+<br><br>
+<a>Ce site a été conçu par une équipe de trois étudiants en BTS Services Informatiques aux Organisations (SIO) option Solutions Logicielles et Applications Métiers (SLAM).</a>
+<br><br>
+<a> Explication du projet : </a>
+<br> </br>
 
-                    <table>
-  <tr>
-    <td>Classe </td>
-    <td>Date</td>
-  </tr>
-  <tr>
-    <td>BTS GPME 1ère année</td>
-
-
-    <td> à définir</td>
-
-  </tr>
-  <tr>
-    <td>BTS SIO 1ère année</td>
-    <td>27mai-6juillet</td>
-
-  </tr>
-  <tr>
-    <td>à définir</td>
-    <td>à définir</td>
-
-  </tr>
-  <tr>
-    <td>à définir</td>
-    <td>à définir</td>
-
-  </tr>
+<a> Le projet à débuter le 12 Janvier 2024. On as fait un cahier des charges et la création d'un MCD.Nous avons rencontrer le client. Bonne visite !
+<br> </br>
+<a>Stage à venir :</a>
+<br> </br>
+<table border = 1>
+    <tr>
+        <td>Classe</td>
+        <td>Date</td>
+    </tr>
+    <tr>
+        <td>BTS GPME 1ère année      </td>
+        <td>à définir</td>
+    </tr>
+    <tr>
+        <td>BTS SIO 1ère année</td>
+        <td>27 mai  - 6 juillet</td>
+    </tr>
+    <tr>
+        <td>à définir</td>
+        <td>à définir</td>
+    </tr>
+    <tr>
+        <td>à définir</td>
+        <td>à définir</td>
+    </tr>
 </table>
+
+</body>
+
 
 <img src="img\NDLP.jpg" style="display: block; margin: 0 auto;">
 
 
-                    
+
+                   
 
         
                 <!-- End of Topbar -->
@@ -410,7 +221,7 @@ SESSION_START();
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Prêt à partir ?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
+                        <span aria-hidden="true"></span>
                     </button>
                 </div>
                 <div class="modal-body">Sélectionnez « Se déconnecter » ci-dessous si vous êtes prêt à mettre fin à votre session en cours.</div>
