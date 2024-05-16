b2d0c841b787577edfdd5cf6d090838ea1a2ed21
MathisLaveille <mathislaveille@ik.me>
2024-05-16 14:07:28 +0200

    blabla

--- index.php
+++ index.php
@@ -106,43 +106,48 @@ mysqli_close($connection);
                 <img src="/img/NDLP.png" width="70" height="50">
             </a>
 
-             <!-- Divider -->
-             <hr class="sidebar-divider my-0">
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
 
-<!-- Nav Item - Dashboard -->
-<li class="nav-item">
-    <a class="nav-link" href="index.php">
-    <img src="/img/Acceuil.png" width="25" height="25"> 
-        <span>Acceuil</span>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a class="nav-link" href="role.php">
-        <img src="/img/role.png" width="35" height="35">
-        <span>Role</span>
-    </a>
-</li>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <img src="/img/Acceuil.png" width="25" height="25">
+                    <span>Acceuil</span>
+                </a>
+            </li>
 
 
-<!-- Heading -->
-<div class="sidebar-heading">
-    Pages
-</div>
+            <!-- Nav Item - Tables -->
+            <li class="nav-item active">
+                <a class="nav-link" href="tables.php">
+                    <img src="img/stage.png" width="25" height="25"></img>
+                    <span>Stages</span></a>
+            </li>
+            
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
 
 
+            <?php
+            if ($user_role == 'ADMIN' || $user_role == 'SUPER_ADMIN') {
+                ?>
 
-<!-- Nav Item - Tables -->
-<li class="nav-item active">
-    <a class="nav-link" href="tables.php">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Stages</span></a>
-</li>
-<!-- Divider -->
-<hr class="sidebar-divider d-none d-md-block">
+                <li class="nav-item">
+                    <a class="nav-link" href="role.php">
+                        <img src="/img/role.png" width="25" height="25">
+                        <span>Role</span>
+                    </a>
+                </li>
 
-<!-- Sidebar Toggler (Sidebar) -->
-<div class="text-center d-none d-md-inline">
+                <?php
+            }
+            ?>
+            
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
         </ul>
@@ -162,8 +167,8 @@ mysqli_close($connection);
                         <i class="fa fa-bars"></i>
                     </button>
 
-                 
-                   
+
+
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -178,15 +183,15 @@ mysqli_close($connection);
 
 
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                                    <?php echo $user_firstname; echo '('.$user_role.')'; ?>
+                                    <?php echo $user_firstname;
+                                    echo '(' . $user_role . ')'; ?>
                                 </span>
 
 
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
-                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -209,65 +214,69 @@ mysqli_close($connection);
                     </ul>
                 </nav>
 
-<body>
+                <body>
 
 
-<div class="card o-hidden border-0 shadow-lg my-5">
-    <div class="card-body p-0">
-        <!-- Nested Row within Card Body -->
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="p-5">
-                    <div class="text-center">
-                        <h1 class="h4 text-gray-900 mb-4">Bienvenue sur le site !</h1>
+                    <div class="card o-hidden border-0 shadow-lg my-5">
+                        <div class="card-body p-0">
+                            <!-- Nested Row within Card Body -->
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Bienvenue sur le site !</h1>
+                                        </div>
+                                        <br><br>
+
+                                        <a style="font-family: Roboto, sans-serif;">Petite présentation :</a>
+                                        <br><br>
+                                        <a style="font-family: Roboto, sans-serif;">Ce site a été conçu par une équipe
+                                            de trois étudiants en BTS Services Informatiques aux Organisations (SIO)
+                                            option Solutions Logicielles et Applications Métiers (SLAM).</a>
+                                        <br><br>
+                                        <a style="font-family: Roboto, sans-serif;">Explication du projet :</a>
+                                        <br><br>
+                                        <a style="font-family: Roboto, sans-serif;">Le projet a débuté le 12 Janvier
+                                            2024. Nous avons fait un cahier des charges et la création d'un MCD. Nous
+                                            avons rencontré le client. Bonne visite !</a>
+                                        <br><br>
+                                        <a style="font-family: Roboto, sans-serif;">Stage à venir :</a>
+                                        <br><br>
+                                        <table border="2">
+                                            <tr>
+                                                <td style="font-family: Roboto, sans-serif;">Classe</td>
+                                                <td style="font-family: Roboto, sans-serif;">Date</td>
+                                            </tr>
+                                            <tr>
+                                                <td style="font-family: Roboto, sans-serif;">BTS GPME 1ère année</td>
+                                                <td style="font-family: Roboto, sans-serif;">à définir</td>
+                                            </tr>
+                                            <tr>
+                                                <td style="font-family: Roboto, sans-serif;">BTS SIO 1ère année</td>
+                                                <td style="font-family: Roboto, sans-serif;">27 mai - 6 juillet</td>
+                                            </tr>
+                                            <tr>
+                                                <td style="font-family: Roboto, sans-serif;">à définir</td>
+                                                <td style="font-family: Roboto, sans-serif;">à définir</td>
+                                            </tr>
+                                            <tr>
+                                                <td style="font-family: Roboto, sans-serif;">à définir</td>
+                                                <td style="font-family: Roboto, sans-serif;">à définir</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <img src="img/NDLP.png" style="max-width: 200px;">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <br><br>
-
-                    <a style="font-family: Roboto, sans-serif;">Petite présentation :</a>
-                    <br><br>
-                    <a style="font-family: Roboto, sans-serif;">Ce site a été conçu par une équipe de trois étudiants en BTS Services Informatiques aux Organisations (SIO) option Solutions Logicielles et Applications Métiers (SLAM).</a>
-                    <br><br>
-                    <a style="font-family: Roboto, sans-serif;">Explication du projet :</a>
-                    <br><br>
-                    <a style="font-family: Roboto, sans-serif;">Le projet a débuté le 12 Janvier 2024. Nous avons fait un cahier des charges et la création d'un MCD. Nous avons rencontré le client. Bonne visite !</a>
-                    <br><br>
-                    <a style="font-family: Roboto, sans-serif;">Stage à venir :</a>
-                    <br><br>
-                    <table border="2">
-                        <tr>
-                            <td style="font-family: Roboto, sans-serif;">Classe</td>
-                            <td style="font-family: Roboto, sans-serif;">Date</td>
-                        </tr>
-                        <tr>
-                            <td style="font-family: Roboto, sans-serif;">BTS GPME 1ère année</td>
-                            <td style="font-family: Roboto, sans-serif;">à définir</td>
-                        </tr>
-                        <tr>
-                            <td style="font-family: Roboto, sans-serif;">BTS SIO 1ère année</td>
-                            <td style="font-family: Roboto, sans-serif;">27 mai - 6 juillet</td>
-                        </tr>
-                        <tr>
-                            <td style="font-family: Roboto, sans-serif;">à définir</td>
-                            <td style="font-family: Roboto, sans-serif;">à définir</td>
-                        </tr>
-                        <tr>
-                            <td style="font-family: Roboto, sans-serif;">à définir</td>
-                            <td style="font-family: Roboto, sans-serif;">à définir</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="p-5">
-                    <div class="text-center">
-                        <img src="img/NDLP.png" style="max-width: 200px;">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
+                </body>
 
                 <!-- End of Topbar -->
 
@@ -306,7 +315,8 @@ mysqli_close($connection);
                         <span aria-hidden="true"></span>
                     </button>
                 </div>
-                <div class="modal-body">Sélectionnez « Se déconnecter » ci-dessous si vous êtes prêt à mettre fin à votre session en cours.</div>
+                <div class="modal-body">Sélectionnez « Se déconnecter » ci-dessous si vous êtes prêt à mettre fin à
+                    votre session en cours.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>
                     <a class="btn btn-primary" href="login.php">Se déconnecter</a>
